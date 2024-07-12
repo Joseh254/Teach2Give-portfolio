bbace41d492ab428ba2b2ea16efb95df58816397
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-07-12 16:13:54 +0300

    create home page and import it in the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Blog from './pages/Blog/Blog'
 import Works from './pages/Works/Works'
 import Contact from './pages/Contact/Contact'
 import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer'
+import Home from './pages/Home/Home'
 
 function App() {
 
@@ -14,11 +16,12 @@ function App() {
     <BrowserRouter>
     <Header/>
     <Routes>
-        <Route path="/" element={<Works/>} />
+        <Route path='/' element={<Home/>}/>
+        <Route path="/Works" element={<Works/>} />
         <Route path="/Blog" element={<Blog />} />
         <Route path="/Contact" element={<Contact />} />
-     
       </Routes>
+      <Footer/>
     </BrowserRouter>
      
     </>
